e6f1c98e4b2194a1f9c7a577b1d8a4ceece5463d
Karol Wydrzyński <vydrsky@gmail.com>
2023-12-27 10:44:24 +0100

    css for cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { CardComponent } from './card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatProgressBarModule,
+    MatCardModule,
+    CardComponent
+  ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
-
+  public cardImageSources: string[] = [
+    "assets/ace_of_spades.png",
+    "assets/ace_of_clubs.png",
+    "assets/ace_of_diamonds.png",
+    "assets/ace_of_hearts.png",
+  ]
 }
