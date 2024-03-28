591c67d94c71d3557c42fcb2dd50b5d1ebf4f43e
Karol Wydrzyński <vydrsky@gmail.com>
2024-03-28 19:31:31 +0100

    validator allows for language specific characters

--- src/app/common/validators/alphanum-validator.ts
+++ src/app/common/validators/alphanum-validator.ts
@@ -2,6 +2,6 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export function alphaNumValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
-    return control.value.match(/[^a-zA-Z0-9]/) ? { error: 'Podany kod zawiera nieprawidłowe znaki.' } : null;
+    return control.value.match(/[-._!"`'#%&,:;<>=@{}~\$\(\)\*\+\/\\\?\[\]\^\|]+/) ? { error: 'Podany kod zawiera nieprawidłowe znaki.' } : null;
   };
 }
